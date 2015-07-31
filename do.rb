c18a45c10910ae34760ec16a99624175998ebf2c
즈눅 <admin@xnu.kr>
2015-08-01 04:24:52 +0900

    moar info

--- do.rb
+++ do.rb
@@ -1,7 +1,13 @@
 #!/usr/bin/env ruby
+require 'base64'
 require 'rake'
 require 'json'
 require 'time'
+require 'zlib'
+
+def deflate(x)
+  Base64.encode64 Zlib::Deflate.deflate(x)
+end
 
 Dir.chdir(File.dirname(__FILE__))
 
@@ -22,15 +28,26 @@ Dir.glob('test/*.sh').each do |path|
     inputpath = "#{testpath}.in"
     sh "sh ./aheui.pre.sh #{testpath}.aheui" if has_pre_script
     output = `timeout 2m /usr/bin/time --format="%S %U" --output=time.tmp ./aheui #{testpath}.aheui #{"< #{inputpath}" if File.exist?(inputpath)}`
-    exitcode = $?.exitstatus
+    output_exitcode = $?.exitstatus
     sh "sh ./aheui.post.sh #{testpath}.aheui" if has_post_script
     timestr = File.read('time.tmp')
     tp = testpath.gsub(/^snippets\//, '')
     if timestr.empty?
       puts "Terminated #{tp}"
+      puts ""
+      puts deflate(output)
+      puts ""
       json_item[tp] = true
-    elsif File.exist?("#{testpath}.exitcode") and File.read("#{testpath}.exitcode").to_i != exitcode or File.read("#{testpath}.out").strip != output.encode('UTF-8', :invalid => :replace).strip
+    elsif File.exist?("#{testpath}.exitcode") and (expected_exitcode = File.read("#{testpath}.exitcode").to_i) != output_exitcode
+      puts "Fail #{tp}: Expected exitcode was #{expected_exitcode}, but it returns #{output_exitcode}"
+      json_item[tp] = false
+    elsif (expected_output = File.read("#{testpath}.out").strip) != (output = output.encode('UTF-8', :invalid => :replace).strip)
       puts "Fail #{tp}"
+      puts ""
+      puts deflate(output)
+      puts "Expected>"
+      puts deflate(expected_output)
+      puts ""
       json_item[tp] = false
     else
       time = timestr.lines[-1].split.map(&:to_f).reduce(&:+)
