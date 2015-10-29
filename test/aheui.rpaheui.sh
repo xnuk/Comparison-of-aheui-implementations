385c9b9177a06770d341930342216de2d8773223
즈눅 <admin@xnu.kr>
2015-10-29 19:53:51 +0900

    hardcoding

--- test/aheui.rpaheui.sh
+++ test/aheui.rpaheui.sh
@@ -1,3 +1,9 @@
 hg clone https://bitbucket.org/pypy/pypy
+
+# Hardcoding, Ho!
+grep -v 'RPYTHON=../pypy/' Makefile > m
+rm Makefile
+mv m Makefile
+
 RPYTHON=pypy/rpython/bin/rpython make
 mv ./aheui-c ../aheui
